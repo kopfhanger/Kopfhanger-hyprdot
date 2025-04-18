5e0a45a06f5c012ba129facd0a874a2d7f811989
kopfhanger <xinyangzengyike@qq.com>
2025-04-18 20:36:12 +0800

    null

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -4,9 +4,9 @@ return {
   { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.color.transparent-nvim" },
 }
